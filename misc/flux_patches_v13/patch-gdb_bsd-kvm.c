9fd46c0a5e7260dc72dd4c822b81568c11519119
John Marino <draco@marino.st>
2024-05-02 19:15:10 +0000

    fix bsd-kvm error

--- misc/flux_patches_v13/patch-gdb_bsd-kvm.c
+++ misc/flux_patches_v13/patch-gdb_bsd-kvm.c
@@ -1,6 +1,16 @@
---- gdb/bsd-kvm.c.orig	2023-07-11 00:50:35 UTC
+--- gdb/bsd-kvm.c.orig	2024-05-02 19:11:06 UTC
 +++ gdb/bsd-kvm.c
-@@ -328,7 +328,9 @@ bsd_kvm_proc_cmd (const char *arg, int f
+@@ -41,7 +41,9 @@
+ #include <paths.h>
+ #include "readline/readline.h"
+ #include <sys/param.h>
++#if !defined(__DragonFly__)
+ #include <sys/proc.h>
++#endif
+ #ifdef HAVE_SYS_USER_H
+ #include <sys/user.h>
+ #endif
+@@ -328,7 +330,9 @@ bsd_kvm_proc_cmd (const char *arg, int f
  #ifdef HAVE_STRUCT_LWP
    addr += offsetof (struct lwp, l_addr);
  #else
